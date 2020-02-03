437dc04d87aaa63a9eb0dbb1cd27b67b9f27e415
Ahmed <imaneben8@gmail.com>
2020-02-03 15:47:47 +0000

    Adding jsx

--- app.js
+++ app.js
@@ -1,16 +1,6 @@
-const title = React.createElement(
-    'h1', {
-        id: 'main-title',
-        title: 'This is a Title.'
-    },
-    'My First React Element!'
-)
+const title = < h1 > My First React Element < /h1>
 
-const desc = React.createElement(
-    'p',
-    null,
-    'I just learnt how to create a React node and render it into the DOM'
-);
+const desc = < p > I just learned how to creare a React node and render it into the DOM < /p>
 
 const header = React.createElement(
     'header',

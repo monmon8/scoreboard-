bd10779027ff78ab813e9415e639197732bf0fb8
Ahmed <imaneben8@gmail.com>
2020-02-11 15:57:18 +0000

    Composing Components

--- app.js
+++ app.js
@@ -13,17 +13,32 @@ const Player = () =>{
             <span className="player-name">
                 Guill
             </span>
-            <div className="counter">
-                <boutton className="counter-action decrement"> - </boutton>
-                <span className="counter-score">35</span>
-                <boutton className="counter-action increment"> + </boutton>
-            </div>
+            <Counter / > 
+        </div>
+    );
+}
+
+const Counter = () => {
+    return (
+        <div className="counter">
+            <boutton className="counter-action decrement"> - </boutton>
+            <span className="counter-score">35</span>
+            <boutton className="counter-action increment"> + </boutton>
+        </div>
+    );
+}
+
+const App = () => {
+    return(
+        <div className="scoreboard">
+        <Header / >
+        {/*player list*/}
+        <Player />
         </div>
     );
 }
 
 ReactDOM.render( 
-    <Player / > ,
-
+    <App / > ,
     document.getElementById('root')
 );

ccd97a105007b1ad35467b5be086ee448d954acb
Ahmed <imaneben8@gmail.com>
2020-08-11 12:42:19 +0100

    Setting and Using Props

--- app.js
+++ app.js
@@ -1,44 +1,39 @@
-const Header = () => {
-    return ( 
-        <header >
-        <h1 > scoreboard </h1>  
-        <span className = "stats" > Players: 1 </span> 
-        </header>
-    );
-}
+const Header = (props) => {
+  return (
+    <header>
+      <h1> {props.title} </h1>
+      <span className="stats"> {props.totalPlayers} </span>
+    </header>
+  );
+};
 
-const Player = () =>{
-    return(
-        <div className="player">
-            <span className="player-name">
-                Guill
-            </span>
-            <Counter / > 
-        </div>
-    );
-}
+const Player = () => {
+  return (
+    <div className="player">
+      <span className="player-name">Guill</span>
+      <Counter />
+    </div>
+  );
+};
 
 const Counter = () => {
-    return (
-        <div className="counter">
-            <boutton className="counter-action decrement"> - </boutton>
-            <span className="counter-score">35</span>
-            <boutton className="counter-action increment"> + </boutton>
-        </div>
-    );
-}
+  return (
+    <div className="counter">
+      <boutton className="counter-action decrement"> - </boutton>
+      <span className="counter-score">35</span>
+      <boutton className="counter-action increment"> + </boutton>
+    </div>
+  );
+};
 
 const App = () => {
-    return(
-        <div className="scoreboard">
-        <Header / >
-        {/*player list*/}
-        <Player />
-        </div>
-    );
-}
+  return (
+    <div className="scoreboard">
+      <Header title="Scoreboard" totalPlayers={1} />
+      {/*player list*/}
+      <Player />
+    </div>
+  );
+};
 
-ReactDOM.render( 
-    <App / > ,
-    document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));

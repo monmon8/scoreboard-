9456f5a0f0ad114b07e73cc793eb5b0be4dd2e45
Ahmed <imaneben8@gmail.com>
2020-08-19 23:27:16 +0100

    adding Keys to Keep Track of Elements

--- app.js
+++ app.js
@@ -2,18 +2,22 @@ const players = [
   {
     name: "Guil",
     score: 50,
+    id: 1,
   },
   {
     name: "Treasure",
     score: 85,
+    id: 2,
   },
   {
     name: "Ashley",
     score: 95,
+    id: 3,
   },
   {
     name: "James",
     score: 80,
+    id: 4,
   },
 ];
 
@@ -51,7 +55,11 @@ const App = (props) => {
       <Header title="Scoreboard" totalPlayers={props.intialPlayers.length} />
       {/*player list*/}
       {props.intialPlayers.map((player) => (
-        <Player name={player.name} score={player.score} />
+        <Player
+          name={player.name}
+          score={player.score}
+          key={player.id.toString()}
+        />
       ))}
     </div>
   );
